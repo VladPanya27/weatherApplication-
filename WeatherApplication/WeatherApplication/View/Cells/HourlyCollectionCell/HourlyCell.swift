1260a631e7a75f1c49a4f18d6c424caadcc450f6
Vlad <vlad.ios.develop@gmail.com>
2022-03-18 14:01:31 +0200

    added tableViewCells

---
+++ WeatherApplication/WeatherApplication/View/Cells/HourlyCollectionCell/HourlyCell.swift
@@ -0,0 +1,23 @@
+//
+//  HourlyCell.swift
+//  WeatherApplication
+//
+//  Created by Vlad Panchenko on 18.03.2022.
+//
+
+import UIKit
+
+class HourlyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
