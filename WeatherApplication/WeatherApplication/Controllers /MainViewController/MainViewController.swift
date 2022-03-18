c947ba07da8a26d92f42cfed1ab7acbf158d237a
Vlad <vlad.ios.develop@gmail.com>
2022-03-18 13:17:29 +0200

    added MainViewController

---
+++ WeatherApplication/WeatherApplication/Controllers /MainViewController/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  WeatherApplication
+//
+//  Created by Vlad Panchenko on 18.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ WeatherApplication/WeatherApplication/Controllers /MainViewController/ViewModelMainController/ViewModelMainController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModelMainController.swift
+//  WeatherApplication
+//
+//  Created by Vlad Panchenko on 18.03.2022.
+//
+
+import Foundation
+
+
+class ViewModelMainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
